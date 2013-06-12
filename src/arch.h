9b0a1be338196a0db67ff6a535080f889ed8db83
if1live <libsora25@gmail.com>
2013-06-12 21:47:58 +0900

    add custom assert

---
+++ src/arch.h
@@ -0,0 +1,79 @@
+// Ŭnicode please 
+#pragma once
+
+// platform detect code
+#undef SR_IOS
+#undef SR_WIN
+#undef SR_ANDROID
+#define SR_IOS 0
+#define SR_WIN 0
+#define SR_ANDROID 0
+
+// platform check macro
+#if __APPLE__
+// iphone check
+// 이것은 가장 밑바닥에 인클루드될 헤더이므로
+// 다른 헤더를 include하지 않는다.
+// #import <Availability.h>
+// #if __IPHONE_2_0
+#undef SR_IOS
+#define SR_IOS 1
+// #endif
+#endif
+
+// android check
+#ifdef __ANDROID__
+#undef SR_ANDROID
+#define SR_ANDROID 1
+#endif
+
+#if (defined _WIN32) || (defined _W64)
+#undef SR_WIN
+#define SR_WIN 1
+#endif
+
+#if SR_IOS == 1 || SR_WIN == 1
+#define SR_USE_PCH 1
+#else
+#define SR_USE_PCH 0
+#endif
+
+// platform define
+#define SR_ARCH_IOS 0x01
+#define SR_ARCH_ANDROID 0x02
+#define SR_ARCH_WIN 0x03
+
+#if SR_IOS
+#define SR_CURR_ARCH SR_ARCH_IOS
+#elif SR_ANDROID
+#define SR_CURR_ARCH SR_ARCH_ANDROID
+#elif SR_WIN
+#define SR_CURR_ARCH SR_ARCH_WIN
+#else
+#error "not valid architecture"
+#endif
+
+
+// dx/gl 사용여부 설정
+#define SR_USE_DX11     0
+#define SR_USE_GL       1
+#define SR_USE_GLES		0
+
+#if SR_USE_GL
+#define SR_IF_GL(EXPR)		EXPR
+#define SR_IF_GLES(EXPR)
+#define SR_IF_DX11(EXPR)    
+#elif SR_USE_GLES
+#define SR_IF_GL(EXPR)      
+#define SR_IF_GLES(EXPR)	EXPR
+#define SR_IF_DX11(EXPR)
+#elif SR_USE_DX11
+#define SR_IF_GL(EXPR)
+#define SR_IF_GLES(EXPR)    EXPR
+#define SR_IF_DX11(EXPR)
+#else
+#define SR_IF_GL(EXPR)
+#define SR_IF_GLES(EXPR)
+#define SR_IF_DX11(EXPR)
+#endif
+
--- src/stdafx.h
+++ src/stdafx.h
@@ -1,8 +1,13 @@
 ﻿// Ŭnicode please 
+#include "arch.h"
+#include "assert_inc.h"
+
+#if SR_USE_PCH
 #include <cmath>
 #include <cstdlib>
 #include <cstdio>
 #include <cassert>
+#include <climits>
 
 #include <iostream>
 #include <sstream>
@@ -14,5 +19,12 @@
 #include <algorithm>
 #include <memory>
 
+#include <unordered_map>
+#include <unordered_set>
+
 #include "irrlicht.h"
 #include <OVR.h>
+
+#else
+#error "NEED PCH"
+#endif
